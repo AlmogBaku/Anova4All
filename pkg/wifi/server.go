89a7dedb5b8b4f4f205c41448705d4c1472d3e3c
almogbaku <almog.baku@gmail.com>
2024-10-03 23:51:30 +0300

    fix: auto-gess localip

--- pkg/wifi/server.go
+++ pkg/wifi/server.go
@@ -47,6 +47,23 @@ func (s *server) HostPort() (string, int) {
 	return s.host, s.port
 }
 
+// GetLocalIP returns the non loopback local IP of the host
+func localIP() (string, error) {
+	addrs, err := net.InterfaceAddrs()
+	if err != nil {
+		return "", err
+	}
+	for _, address := range addrs {
+		// check the address type and if it is not a loopback the display it
+		if ipnet, ok := address.(*net.IPNet); ok && !ipnet.IP.IsLoopback() {
+			if ipnet.IP.To4() != nil {
+				return ipnet.IP.String(), nil
+			}
+		}
+	}
+	return "", fmt.Errorf("no local IP address found")
+}
+
 func (s *server) start() error {
 	addr := fmt.Sprintf("%s:%d", s.host, s.port)
 	listener, err := net.Listen("tcp", addr)
@@ -57,7 +74,7 @@ func (s *server) start() error {
 	s.listener = listener
 	log.Printf("Serving on %s", addr)
 	if s.host == "" {
-		s.host = listener.Addr().String()
+		s.host, _ = localIP()
 	}
 
 	s.wg.Add(1)
